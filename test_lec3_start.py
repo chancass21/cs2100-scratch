20595b173dd2df2686d76819427d6502a6b11bbc
Cassidy Chan <chan.cass@northeastern.edu>
2026-01-12 10:10:43 -0500

    other_tests

--- test_lec3_start.py
+++ test_lec3_start.py
@@ -10,12 +10,17 @@ from lec3_start import generate_mileage_stats
 
 class TestRunners(unittest.TestCase):
     ''' test the functions defined in lec3_v2_dictionaries '''
+
     def test_generate_milage_stats(self) -> None:
         test_lst = [1, 2, 3]
         expected = {"total miles" : 6, "avg miles" : 2}
-        actual = generated_mielage_stats(test_lst)
-        self.assertEqaul(expected, actual)
-        
+        actual = generate_mileage_stats(test_lst)
+        self.assertEqual(expected, actual)
+
+        # other tests
+        self.assertEqual({"total miles" : 0, "avg daily" : 0}, 
+                         generate_milage_stats([0]))
+
         # self.assertEqual(functioncall, expected output)
         # self.assertEqual(functioncall, expected output)
         # self.assertEqual(functioncall, expected output)
