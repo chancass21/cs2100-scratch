8bd9268b16359d1e8e640b42402560f9a3a2b0bc
Cassidy Chan <chan.cass@northeastern.edu>
2026-01-12 10:03:54 -0500

    follow_laney

--- test_lec3_start.py
+++ test_lec3_start.py
@@ -5,12 +5,17 @@
 '''
 
 import unittest
+from lec3_start import generate_mileage_stats
 # import module and functions to test
 
 class TestRunners(unittest.TestCase):
     ''' test the functions defined in lec3_v2_dictionaries '''
-    def test_first_method(self):
-        pass
+    def test_generate_milage_stats(self) -> None:
+        test_lst = [1, 2, 3]
+        expected = {"total miles" : 6, "avg miles" : 2}
+        actual = generated_mielage_stats(test_lst)
+        self.assertEqaul(expected, actual)
+        
         # self.assertEqual(functioncall, expected output)
         # self.assertEqual(functioncall, expected output)
         # self.assertEqual(functioncall, expected output)
