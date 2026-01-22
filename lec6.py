b65f67a3e5b79c91bf0f226a0e7802a3c60869d0
Cassidy Chan <chan.cass@northeastern.edu>
2026-01-22 09:49:25 -0500

    wtf

--- lec6.py
+++ lec6.py
@@ -20,8 +20,15 @@ s_a = {(1, 2), (3, 4)} # set of tuples
 # Below are unittests; write a function that would pass the tests. 
 # Don't worry about edge cases or documentation. 
 
+def list_to_sets(l: list[list[str]]) -> dict[str, set[str]]:
+    """
+    Transform 2D list of strings into a dictionary where keys are strings
+    and values are sets
+    """
+    pass
+
 start_list = [["a"], ["b"], ["c"]]
-expected = {"a" : set(), "b" : set(), "c" : set()}
+expected = {"a": set(), "b": set(), "c": set()}
 actual = list_to_sets(start_list)
 self.assertEqual(expected, actual)
 
@@ -35,5 +42,3 @@ start_list2 = [["a", "b", "c"], ["d", "e", "f"]]
 expected2 = {"a" : {"b", "c"}, "d" : {"e", "f"}}
 actual2 = list_to_sets(start_list)
 self.assertEqual(expected2, actual2)
-
-def list_to_sets(lst: list[list[str]]) -> dict[str, set[str]]:
