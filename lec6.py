a76bfca997fb4b5261cb75579281c21ff03e109f
Cassidy Chan <chan.cass@northeastern.edu>
2026-01-22 09:34:04 -0500

    # comments

--- lec6.py
+++ lec6.py
@@ -2,13 +2,14 @@
 
 empty_set = set()
 s = {1, 2, 3}
-# can't use an empty curly brackets because its considered a definition
 s.add(4)
 s.remove(2)
 
 # s[0] = 4 doesn't work
 # s now has {1, 3, 4}
 
-s = {(1, 2), (3, 4)}
+s = {(1, 2), (3, 4)} # set of tuples
 
 # can't create a list of sets
+# can't create a set of sets
+# can't use an empty curly brackets because its considered a definition
